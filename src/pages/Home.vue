109579150c8b9ba34ce261806bd10997b7c3f9db
everlasting2002 <851627835@qq.com>
2022-10-19 21:38:06 +0800

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { ref, defineComponent } from 'vue'
+import Btn from "../components/Btn.vue";
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="main-page">
+    <img
+      :src="`/assets/nilou.jpg`"
+      alt="logo"
+      class="logo"
+    />
+
+    <div class="title">虚空劫灰往事书</div>
+    <Btn
+      @click="$router.push('joinRoom')"
+      content="加入房间"
+    ></Btn>
+    <Btn
+      @click="$router.push('createRoom')"
+      content="创建房间"
+    ></Btn>
+    <Btn @click="$router.push('review')" content="游戏记录"></Btn>
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+  @font-face {
+    font-family: gete;
+    src: url("../../public/assets/gete.ttf");
+  }
+
+  .main-page {
+    .logo {
+      padding-top: 10vh;
+      width: 18vh;
+      margin: auto;
+      display: block;
+    }
+
+    .title {
+      margin: 7vh;
+      text-align: center;
+      font-size: 3rem;
+      font-weight: bolder;
+      font-family: gete;
+    }
+
+    .btn {
+      display: block;
+      margin: 5vh auto;
+      text-align: center;
+    }
+  }
+</style>
