386018a9efa88281766cacc1ce76dd950611925c
everlasting2002 <851627835@qq.com>
2022-10-25 19:52:40 +0800

    添加入口页面

---
+++ src/pages/Entrance.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ref, defineComponent } from 'vue'
+import Btn from "../components/Btn.vue";
+
+</script>
+
+<template>
+  <div class="main-page">
+    <img
+      :src="`/assets/paimon.gif`"
+      alt="logo"
+      class="logo"
+    />
+
+    <div class="title">虚空劫灰往世书</div>
+    <Btn @click="$router.push('home')" content="点击开始"></Btn>
+  </div>
+  
+</template>
+
+<style lang="scss" scoped>
+
+  .main-page {
+    .logo {
+      padding-top: 10vh;
+      width: 18vh;
+      margin: auto;
+      display: block;
+    }
+
+    .title {
+      margin: 7vh;
+      text-align: center;
+      font-size: 3rem;
+      font-weight: bolder;
+    }
+
+    .btn {
+      display: block;
+      margin: 5vh auto;
+      text-align: center;
+    }
+  }
+</style>
