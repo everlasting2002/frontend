ab5b659dfa94c73ea8ba81006931d5b2e25c3d92
everlasting2002 <851627835@qq.com>
2022-10-27 03:10:09 +0800

    添加房间内玩家列表组件

---
+++ src/components/RoomPlayerList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="room-player-list">
+    <div
+      v-for="item in playerList"
+      :key="item.index"
+      class="player"
+    >
+      <div class="index">
+        <span class="index-content">{{ item.index }}</span>
+      </div>
+      <div class="name">{{item.name}}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, computed, PropType } from "vue";
+  import { PublicPlayerDef } from "../../shared/ModelDefs";
+  const RoomPlayerList = defineComponent({
+    name: "RoomPlayerList",
+    props: {
+      playerList: {
+        type: Object as PropType<PublicPlayerDef[]>,
+        required: true,
+      },
+    },
+    components: {},
+  });
+
+  export default RoomPlayerList;
+</script>
+
+<style lang="scss">
+
+</style>
