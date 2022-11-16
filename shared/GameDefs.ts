fedc72920b9c621d4ad3daf1ba9d1547c918bff9
everlasting2002 <851627835@qq.com>
2022-11-17 01:37:21 +0800

    身份卡片增加了身份说明

--- shared/GameDefs.ts
+++ shared/GameDefs.ts
@@ -28,14 +28,14 @@ export const ChineseNames: Record<Character, string> = {
 
 export const CharacterIntro: Record<Character, string> = {
 	MERLIN: "你可以看到所有坏那菈。",
-	PERCIVAL: "你可以看到"+ChineseNames.MERLIN+"和"+ChineseNames.MORGANA+"，但是不知道两个玩家的具体身份。",
-	ASSASSIN: "你随时可以进行刺杀，或是任务成功三次后进行刺杀，进行刺杀后游戏立即结束。如果成功刺杀了"+ChineseNames.MERLIN+"，那么坏那菈获得胜利，否则好那菈获得胜利。",
-	MORDRED: ChineseNames.MERLIN+"看不到你。",
+	PERCIVAL: "你可以看到【"+ChineseNames.MERLIN+"】和【"+ChineseNames.MORGANA+"】，但是不知道两个玩家的具体身份。",
+	ASSASSIN: "你随时可以进行刺杀。如果成功刺杀了【"+ChineseNames.MERLIN+"】，那么坏那菈获得胜利，否则好那菈获得胜利。",
+	MORDRED: "【"+ChineseNames.MERLIN+"】看不到你。",
 	OBERON: "你无法看到其他坏那菈，其他坏那菈也看不到你。",
-	MORGANA: "你需要假扮成"+ChineseNames.MERLIN+"，迷惑"+ChineseNames.PERCIVAL+"。",
+	MORGANA: "你需要假扮成【"+ChineseNames.MERLIN+"】，迷惑【"+ChineseNames.PERCIVAL+"】。",
 	LOYALIST: "好那菈",
 	MINIONS: "坏那菈",
-	FAIRY: "在第2、3、4轮任务结束后，"+ChineseNames.FAIRY+"可以把一位玩家的身份悄悄告诉你，随后"+ChineseNames.FAIRY+"被查验的玩家拥有。",
+	FAIRY: "在第2、3、4轮任务结束后，【"+ChineseNames.FAIRY+"】可以把一位玩家的身份悄悄告诉你，随后【"+ChineseNames.FAIRY+"】被查验的玩家拥有。",
 	"": "",
 };
 
--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -1,19 +1,41 @@
 <template>
-	<Avatar class="character" :character="self.character"></Avatar>
+	<!-- 下面这块应该是个组件，现在凑合看身份用的 -->
+	<div class="card">
+		<Avatar class="character" :character="self.character"></Avatar>
+		<p class="intro">
+			你是：{{ChineseNames[self.character]}}<br/>
+			{{CharacterIntro[self.character]}}
+		</p>
+	</div>
 </template>
 
 <script setup lang="ts">
 	import Btn from "../components/Btn.vue";
 	import Avatar from "../components/Avatar.vue";
 	import { players,self,Room } from "../reactivity/game";
+	import { CharacterIntro, ChineseNames } from "../../shared/GameDefs";
 
 </script>
 
 <style lang="scss" scoped>
-	.character{
+	.card{
 		position: absolute;
+		height: calc(21/100*var(--width));
+		width: calc(14/100*var(--width));
 		right: calc(25/100*var(--width));
 		bottom: calc(10/100*var(--height));
 		z-index: 2;
+		border: 2px;
+		border-color: green;
+		border-style: outset;
+		padding: 0.5em;
+		//下面这些也应该在组件里实现。
+		.character{
+			width: 100%;
+		}
+		.intro{
+			position: absolute;
+			
+		}
 	}
 </style>
