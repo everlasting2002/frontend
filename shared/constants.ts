5d32eb619dc15a764fb7e9064cd182be044b4e7b
everlasting2002 <851627835@qq.com>
2022-12-06 16:28:19 +0800

    更新后端链接

--- shared/constants.ts
+++ shared/constants.ts
@@ -1,3 +1,3 @@
-export const SERVER_DOMAIN = import.meta.env.VITE_BACKEND_DOMAIN ? import.meta.env.VITE_BACKEND_DOMAIN : "localhost";
+export const SERVER_DOMAIN = import.meta.env.VITE_BACKEND_DOMAIN ? import.meta.env.VITE_BACKEND_DOMAIN : (import.meta.env.DEV ? "localhost" : "genshin.kernelbin.cn:8081");
 
 export const WS_PATH = "ws://" + SERVER_DOMAIN + "/api";
